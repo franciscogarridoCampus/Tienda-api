af80d7450b5c5cddecf5e7b63e8b16e3814d52a5
Adrián.Álvarez <adrian.alvarez@eusa.es>
2025-10-31 14:31:28 +0100

    mmm

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ const app = express();
 app.use(express.json());
 // Importar rutas
 app.use('/productos', require('./routes/productosRoutes'));
-/*
 app.use('/carritos', require('./routes/carritosRoutes'));
 app.use('/categorias', require('./routes/categoriasRoutes'));
 app.use('/clientes', require('./routes/clientesRoutes'));
 app.use('/pedidos', require('./routes/pedidosRoutes'));
 app.use('/proveedores', require('./routes/proveedoresRoutes'));
-*/
 //definir el resto de routes
 //Mejora solicitada, guardar en un log de json todas las llamadas a la API
 app.listen(3000, () => console.log('Servidor escuchando en http://localhost:3000'));
