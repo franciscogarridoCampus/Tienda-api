89be7d9d5d56863d2b2b1e4334d8aef534456990
Francisco.Garrido <franciscogarrido.24@campuscamara.es>
2025-10-31 14:08:11 +0100

    productoFuncionando

--- routes/productosRoutes.js
+++ routes/productosRoutes.js
@@ -1,5 +1,5 @@
 //esta clase os la doy completa. Es la encargada de asignar la ruta al controlador, de esta
-//forma separamos las responsabilidades→ SOLID→ Principio de responsabilidad.
+
 const express = require('express');
 const router = express.Router();
 const productosController = require('../controllers/productosController');
@@ -10,4 +10,3 @@ router.put('/:id', productosController.actualizar);
 router.delete('/:id', productosController.eliminar);
 module.exports = router;
 //Las demás rutas (categoriasRoutes.js, clientesRoutes.js, etc.) tendrán la
-//misma estructura, cambiando solo los nombres de los controladores.
